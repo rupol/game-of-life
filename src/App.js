c9af9a88fbf4deebe45d838888b79b6bcf353606
Ruth Poliakon <r.poliakon@gmail.com>
2020-07-21 18:26:38 -0600

    remove CRA defaults, create basic file structure
    install react router and hook up about and game components to navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, NavLink } from "react-router-dom";
+
+import GameOfLife from "./components/GameOfLife";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <NavLink exact to="/">
+          Game of Life
+        </NavLink>
+        <NavLink exact to="/about">
+          About
+        </NavLink>
+      </nav>
+      <Route exact path="/" component={GameOfLife} />
+      <Route exact path="/about" component={About} />
     </div>
   );
 }
---
+++ src/components/GameOfLife.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GameOfLife() {
+  return (
+    <div className="game-of-life">
+      <h1>Conway's Game of Life</h1>
+    </div>
+  );
+}
+
+export default GameOfLife;
